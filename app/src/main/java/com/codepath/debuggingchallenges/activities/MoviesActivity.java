06b4015658ed97fd84f5ba6a2970d81ddfca6600
NJSal <salazar.davidf@gmail.com>
2022-02-13 22:22:13 -0500

    commit: milestone4 fixed MoviesActivity
    onCreate: 1.initialize array, adapter initialized, set initz'd adapter, set layoutmanager
    
    fetchMovies: attach api key to url, add all movies to array list, movies.
    
    adapter data set changed, and tested with log statements

--- app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
@@ -32,27 +33,48 @@ public class MoviesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        //RecyclerView rvMovies;
+        //MoviesAdapter adapter;                          //adapter must be known to fetchMovies() function
+
         rvMovies = findViewById(R.id.rvMovies);
 
+        //issue1: must initialize array before using it
+        movies = new ArrayList<>();
+
+        //issue 2: make use of adapter, adapter has already been initialized
+        adapter = new MoviesAdapter(movies);        //MoviesAdapter constructor only takes 1 parameter
+
+        //fixed following (issue 2): above
         // Create the adapter to convert the array to views
-        MoviesAdapter adapter = new MoviesAdapter(movies);
+        //MoviesAdapter adapter = new MoviesAdapter(movies);
 
         // Attach the adapter to a ListView
         rvMovies.setAdapter(adapter);
+
+        //issue 3: set the LayOutManger
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         fetchMovies();
     }
 
 
     private void fetchMovies() {
-        String url = " https://api.themoviedb.org/3/movie/now_playing?api_key=";
+        String url = " https://api.themoviedb.org/3/movie/now_playing?api_key="+API_KEY;        //API_KEY must be part of the URL
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON response) {
+                Log.d(TAG, "onSuccess");
+                //JSONObject response = json.jsonObject;
                 try {
                     JSONArray moviesJson = response.jsonObject.getJSONArray("results");
-                    movies = Movie.fromJSONArray(moviesJson);
+                    Log.i(TAG, "results " + moviesJson.toString());
+                    //movies = Movie.fromJSONArray(moviesJson);
+
+                    movies.addAll(Movie.fromJSONArray(moviesJson));
+                    adapter.notifyDataSetChanged();
+                    Log.i(TAG, "Movies: " + movies.size());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
